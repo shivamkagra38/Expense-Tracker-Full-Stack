ea03385dcdb63534a5294f9c318f2216f05b28f2
Shivam Kagra <shivamkagra38@gmail.com>
2025-10-21 19:15:16 +0530

    react router dom integerated

--- Frontend/src/index.js
+++ Frontend/src/index.js
@@ -1,6 +1,28 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router";
+
 
 const mainRoot = ReactDOM.createRoot(document.getElementById("root"));
 
-mainRoot.render("Hello World!");
+//Routing configguration
+const routerConfig = createBrowserRouter([
+
+    {
+        path: "/",
+        element:"Home component to be rendered here.",
+        errorElement: "Invalid route"
+    },
+    {
+        path: "/login",
+        element: "Login component"
+    },
+    {
+        path: "/signup",
+        element: "signup component"
+    }
+
+]);
+
+//Rendering...
+mainRoot.render(<RouterProvider router={routerConfig} />);
