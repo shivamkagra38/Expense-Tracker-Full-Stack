ab1feb35983814ce2f45b34c5f5426352b173151
shivamkagra38 <83065622+shivamkagra38@users.noreply.github.com>
2025-11-16 19:20:37 +0530

    Change SameSite attribute for token cookie

--- Backend/Controllers/loginUser.js
+++ Backend/Controllers/loginUser.js
@@ -46,7 +46,7 @@ const loginUser = async (req, res) => {
         const token = await jwt.sign(tokenData, process.env.SECRET_KEY, {expiresIn: "1d"});
         
         res.status(200)
-        .cookie("token", token, {maxAge:1*24*60*60*1000, httpOnly:true, sameSite:"strict"})
+        .cookie("token", token, {maxAge:1*24*60*60*1000, httpOnly:true, sameSite:"None"})
         .json({
             message:`Welcome ${userExists.fullname}`,
             user: {
