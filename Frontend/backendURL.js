26d3a54a44c04b0fa4f5333b67788b15680df39a
shivamkagra38 <83065622+shivamkagra38@users.noreply.github.com>
2025-12-10 20:22:46 +0530

    Update backend URL to new production endpoint

--- Frontend/backendURL.js
+++ Frontend/backendURL.js
@@ -1,5 +1,5 @@
-const backendURL = "https://expense-tracker-back-3nz0.onrender.com";
+//const backendURL = "https://expense-tracker-back-3nz0.onrender.com";
 //const backendURL = "http://localhost:8000";
-
+ const backendURL = "https://expense-tracker-full-stack-production-adc0.up.railway.app"
 
 export default backendURL;
